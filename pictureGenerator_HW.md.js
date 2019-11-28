89c5a3d6cd47bc93ee35a0ac92342e27be1a0298
ahmadalarif <44748381+ahmadalarif@users.noreply.github.com>
2019-11-28 15:12:23 +0300

    Done for pictureGenerator

---
+++ pictureGenerator_HW.md.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+let image =document.createElement("img");{
+image.setAttribute("src","https://source.unsplash.com/1600x900");
+let x = document.querySelector("div.image");
+x.appendChild(image);
+}
+
+buttonRandomize.appendChild("click", displayImage);
+
+
