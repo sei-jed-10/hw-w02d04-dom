c1a40c00d474f4bf12667e6fbb01186a1f09c7a8
fatmake <fatmakelantan@outlook.com>
2019-11-28 08:21:35 +0300

    HW7

---
+++ aboutme-script.js
@@ -0,0 +1,13 @@
+
+$('document').ready(function(){
+	$('body').css('font-family','Arial, sans-serif');
+
+	$('#nickname').text('ima');
+	$('#favorite').text('elephant');
+	$('#hometown').text('makkah');
+
+	$('li').addClass('listitem');
+	$('li').css('color','rebeccapurple');
+	$('.profilePicture').after('<img src = "images/logo.png" hieght=286 width=286>');
+
+});
---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function(){
+	
+	$('#randomize').click(function(){
+		
+		$('.img').after("src","https://source.unsplash.com/random");
+
+	});
+
+});
+
+//<img src= "https://source.unsplash.com/random" width ="490" hieght="736">
