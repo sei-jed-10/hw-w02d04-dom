df2ae1bcb6f6844b3b4547b0f61f63407d42b80a
Sarah <alahmadisr@gmail.com>
2019-11-28 05:16:40 +0300

    W02D04 -HW

--- Picture Generator/pg.js
+++ Picture Generator/pg.js
@@ -1,13 +1,13 @@
 
 $(document).ready(function(){
+  $("body").prepend("<P> Please specify the image's width and height:</p>")
   $("#randomize").click(function(){
     $("div.image").empty();
     var width=$("input.width").val();
     var height=$(".height").val();
     var img = $('<img>');
     img.attr('src','https://source.unsplash.com/random')
-    img.css('height',height);
-    img.css({'width':width})
+    img.css({'width':width,'height':height})
     $("div.image").append(img);
   });
 });
