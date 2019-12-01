dd8471a157cc61f0a2f900021d861006c7329d97
Abdullah Binmahfouz <abinm670@mtroyal.ca>
2019-12-01 09:22:10 +0300

    Dom with JS

--- getImg.js
+++ getImg.js
@@ -1,11 +1,3 @@
-/** 
- * Change the font-family of the page to "Arial, sans-serif"
-Replace each of the span tags (nickname, favorite, hometown) with your own information (e.g. put your nickname in the nickname span)
-Give each li the class of "listitem"
-Change each li's text color to "rebeccapurple"
-Create a new img element and set its src attribute to a picture of you. Put that new img element in the div with the class of "profilePicture"
-*/
-
 window.onload = function() {
   // /* get the element*/
 
@@ -22,20 +14,16 @@ window.onload = function() {
   newImg.setAttribute("id", "changeSize");
 
   //get the value from the input
-  //   var width1 = input.getAttribute("width");
-  //   var hight1 = input.getAttribute("hight");
+  var width1 = input.getAttribute("width");
+  var hight1 = input.getAttribute("hight");
 
   var myButton = document.querySelector("#randomize");
 
   myButton.addEventListener("click", function() {
-    node element add new src with new src atribu that been created.
+    //node element add new src with new src atribu that been created.
     document.getElementById("changeSize").width = width1;
     document.getElementById("changeSize").hight = hight1;
     newImg.setAttribute("src", "https://source.unsplash.com/1600x900");
   });
   // };
 };
-
-// var body = body.getAttribute(style);
-// //
-// alert(body);
